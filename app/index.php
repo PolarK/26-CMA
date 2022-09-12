224ffa9e237b879a3c6533136c10768c784c95df
Justin San <santosojustin@gmail.com>
2022-09-13 04:52:36 +1000

    Update index.php

--- app/index.php
+++ app/index.php
@@ -10,11 +10,22 @@ if (isset($_SESSION['available'])) {
 <div id="content" class="container-fluid p-5">
 
     <?php
-    // check if session is set
-    if (isset($_SESSION['available'])) {
-        include('./src/pages/dashboard.php');
-    } else {
-        include('./src/pages/login.php');
+    switch ($_GET['page']) {
+        case 'register':
+            include('./src/pages/register.php');
+            break;
+
+        case 'login':
+            include('./src/pages/login.php');
+            break;
+        
+        default:
+            if (isset($_SESSION['available'])) {
+                include('./src/pages/dashboard.php');
+            } else {
+                include('./src/pages/login.php');
+            }
+            break;
     }
     ?>
 

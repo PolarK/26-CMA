cfe2f8a80622aca3d13828dfedc93bcfbb5292f9
Justin San <santosojustin@gmail.com>
2022-09-13 02:58:20 +1000

    Updated testPassword (much stronger security)

--- app/testPassword.php
+++ app/testPassword.php
@@ -11,9 +11,9 @@ if (isset($_POST['resetSession'])) {
 
 <strong>Create Simple Account</strong>
 <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST" enctype="multipart/form-data">
-    <p> Username: <input type="text" name="username"> </p>
-    <p> Password: <input type="text" name="password"> </p>
-    <p> Re-Enter Password: <input type="text" name="passwordRepeat"> </p>
+    <p> Username: <input type="text" name="username" value="ASAJ0001"> </p>
+    <p> Password: <input type="text" name="password" value="T3stP@$$"> </p>
+    <p> Re-Enter Password: <input type="text" name="passwordRepeat" value="T3stP@$$"> </p>
     <input type="submit" name="createAccount" value="Register" />
 </form>
 
@@ -26,10 +26,12 @@ if (isset($_POST['createAccount'])) {
     $passwordRepeat = $_POST['passwordRepeat'];
 
     if (($password === $passwordRepeat) && (isset($_POST['username']))) {
-        $hashPass = password_hash($password, PASSWORD_DEFAULT);
+        $salt = hash('SHA512', microtime(true).mt_rand(1000,9000));
+        $hashPass = hash('SHA512', $salt.$username.$password);
 
         $_SESSION['username'] = $username;
         $_SESSION['password'] = $password;
+        $_SESSION['salt'] = $salt;
         $_SESSION['hashPass'] = $hashPass;
     } else {
         echo "Password does not match.";
@@ -39,6 +41,7 @@ if (isset($_POST['createAccount'])) {
 if (isset($_SESSION['password'])) {
     echo "username: <pre>" . $_SESSION['username'] . "</pre>";
     echo "password: <pre>" . $_SESSION['password'] . "</pre>";
+    echo "salt: <pre>" . $_SESSION['salt'] . "</pre>";
     echo "hashed password: <pre>" . $_SESSION['hashPass'] . "</pre>";
 }
 ?>
@@ -53,8 +56,20 @@ if (isset($_SESSION['password'])) {
 
 <?php
 
+function verifyAccount($uname, $upass){
+    //* Have to grab Salt & Hash from DB. This is just an example...
+    $salt = $_SESSION['salt'];
+    $hash = $_SESSION['hashPass'];
+
+    echo hash('SHA512', $salt.$uname.$upass);
+
+    return hash('SHA512', $salt.$uname.$upass) == $hash;
+}
+
 if (isset($_POST['verifyAccount'])) {
-    if ($_POST['vUsername'] == $_SESSION['username'] && password_verify($_POST['vPassword'], $_SESSION['hashPass'])) {
+
+
+    if ($_POST['vUsername'] == $_SESSION['username'] && verifyAccount($_POST['vUsername'], $_POST['vPassword'])) {
         echo "<p style='color:green'> Password is correct.</p>";
     } else {
         echo "<p style='color:red'> Password is incorrect.</p>";

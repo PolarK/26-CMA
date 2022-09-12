f8ef7d0830e818bf25609530cef01d1e9f345a98
Justin San <santosojustin@gmail.com>
2022-09-13 04:53:00 +1000

    Disabling jquery register page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
     // Load register form content when 'register' is clicked
+/*  
     $('#displayRegisterForm').click(function(){
        $('#content').load('src\\pages\\register.php');
     });
-
+*/
     $('form input').blur(function(){
         if(!$(this).val()){
             $(this).css('border','1px solid red');

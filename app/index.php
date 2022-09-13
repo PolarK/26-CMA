9695f930e39f142f394eac5ad2c3d4d358dac0e4
Justin San <santosojustin@gmail.com>
2022-09-13 19:45:26 +1000

    Update index.php

--- app/index.php
+++ app/index.php
@@ -2,32 +2,52 @@
 include('./src/template/header.php');
 
 
-if ($_SESSION['available']) {
-    require __DIR__ . './src/template/navbar.php';
-    require __DIR__ . '/src/pages/dashboard.php';
-    include('./src/template/footer.php');
-    exit();
-}
-
 $request = $_SERVER['REQUEST_URI'];
 
-switch ($request) {
-    case '/':
-        require __DIR__ . '/src/pages/login.php';
-        break;
 
-    case '':
-        require __DIR__ . '/src/pages/login.php';
-        break;
+//! Where most of pages will be on
+if ($_SESSION['available']) {
+    include('./src/template/navbar.php');
 
-    case '/register':
-        require __DIR__ . '/src/pages/register.php';
-        break;
+    switch ($request) {
+        case '/':
+            require __DIR__ . '/src/pages/dashboard.php';
+            break;
 
-    default:
-        http_response_code(404);
-        require __DIR__ . '/src/pages/error/404.php';
-        break;
+        case '':
+            require __DIR__ . '/src/pages/dashboard.php';
+            break;
+
+        case '/dashboard':
+            require __DIR__ . '/src/pages/dashboard.php';
+            break;
+
+        default:
+            http_response_code(404);
+            require __DIR__ . '/src/pages/error/404.php';
+            break;
+    }
+} else {
+
+    switch ($request) {
+        case '/':
+            require __DIR__ . '/src/pages/login.php';
+            break;
+
+        case '':
+            require __DIR__ . '/src/pages/login.php';
+            break;
+
+        case '/register':
+            require __DIR__ . '/src/pages/register.php';
+            break;
+
+        default:
+            http_response_code(404);
+            require __DIR__ . '/src/pages/error/404.php';
+            break;
+    }
 }
+
 
 include('./src/template/footer.php');

29c94b43cd85a41027c19ce12fa455e188bd5062
Justin San <santosojustin@gmail.com>
2022-09-19 20:02:06 +1000

    Update checkSubmission.php

--- src/pages/submitter/checkSubmission.php
+++ src/pages/submitter/checkSubmission.php
@@ -1,3 +1,30 @@
-<?php 
-echo "CHECK SUBMISSION";
+<?php
+include_once("./classes/components/card.php");
+include_once("./classes/dbAPI.class.php");
+
 ?>
+
+<!--CONTENT START-->
+<div id="content" class="container-fluid p-5">
+    <div class="d-flex flex-column justify-content-center align-items-center text-center h-100">
+        <h1 class="display-4">My Submissions</h1>
+        <p class="lead">Check your paper progress here. We'll send you a notification as soon as we completed our review!</p>
+        <div style="margin: auto; width: 36rem;">
+
+            <?php
+            // !testing only, will have to get data from the db
+            $subData = [
+                'Future-of-IoT.pdf',
+                'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem nostrum assumenda provident perspiciatis, vel quaerat aut fugiat perferendis magni explicabo praesentium totam, in commodi quidem, exercitationem ab! Vero, voluptatibus laborum.',
+                '/file/' . $_SESSION['UID'] . '/Future-of-IoT.pdf',
+                'Accepted',
+                '19-09-2022 7:10PM'
+            ];
+
+            echo Card::display("submission", $subData);
+
+            ?>
+        </div>
+    </div>
+</div>
+<!--CONTENT END-->

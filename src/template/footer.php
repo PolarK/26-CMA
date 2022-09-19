b03f0da1221df180d1b80a42c01451bb5b12bc78
Justin San <santosojustin@gmail.com>
2022-09-19 22:51:29 +1000

    Added TableEdit Library

--- src/template/footer.php
+++ src/template/footer.php
@@ -6,6 +6,7 @@
             </em>
     </div>
 </footer>
+<script src="scripts/libraries/jquery-tabledit-1.2.3/jquery.tabledit.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 

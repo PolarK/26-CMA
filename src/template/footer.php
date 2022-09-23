d54e8fcaa09b3eed11cfc279a8d83afffe95ee42
Justin San <santosojustin@gmail.com>
2022-09-23 22:18:00 +1000

    Added Toast Library

--- src/template/footer.php
+++ src/template/footer.php
@@ -7,6 +7,7 @@
     </div>
 </footer>
 <script src="scripts/libraries/jquery-tabledit-1.2.3/jquery.tabledit.min.js"></script>
+<script src="scripts\libraries\jquery-toast-plugin\dist\jquery.toast.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
--- src/template/header.php
+++ src/template/header.php
@@ -9,6 +9,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="src\styles\bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <link rel="stylesheet" href="scripts\libraries\jquery-toast-plugin\dist\jquery.toast.min.css">
+
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="scripts\main.js"></script>
 </head>

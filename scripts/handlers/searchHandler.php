f9bcc553c82a95bfa87a6778f1895386cee10861
Justin San <santosojustin@gmail.com>
2022-09-23 14:23:46 +1000

    Managed User Search completed

--- scripts/handlers/searchHandler.php
+++ scripts/handlers/searchHandler.php
@@ -1,6 +1,8 @@
 <?php
 include_once "../../classes/dbAPI.class.php";
 
+
+
 $db = new Database();
 
 function displayUsers($rawData)
@@ -18,13 +20,42 @@ function displayUsers($rawData)
     }
 }
 
+/* START USER SEARCH */
+
 if (isset($_POST['searchByUID'])) {
     $users = $db->findUserById($_POST['searchByUID']);
     displayUsers($users);
 }
 
 if (isset($_POST['searchByFName'])) {
-    $users = $db->findUserById($_POST['searchByFName']);
+    $users = $db->findUserByFirstName($_POST['searchByFName']);
     displayUsers($users);
 }
+
+if (isset($_POST['searchByLName'])) {
+    $users = $db->findUserByLastName($_POST['searchByLName']);
+    displayUsers($users);
+}
+
+if (isset($_POST['searchByDOB'])) {
+    $users = $db->findUserByDOB($_POST['searchByDOB']);
+    displayUsers($users);
+}
+
+if (isset($_POST['searchByEmail'])) {
+    $users = $db->findUserByEmail($_POST['searchByEmail']);
+    displayUsers($users);
+}
+
+if (isset($_POST['searchByPhoneNo'])) {
+    $users = $db->findUserByPhoneNo($_POST['searchByPhoneNo']);
+    displayUsers($users);
+}
+
+if (isset($_POST['searchByRole'])) {
+    $users = $db->findUserByRole($_POST['searchByRole']);
+    displayUsers($users);
+}
+
+/* END USER SEARCH */
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-function displayUsers(){
+function displayUsers() {
     $('#displayUsers').Tabledit({
         url: 'scripts/handlers/userHandler.php',
         columns: {
@@ -10,7 +10,7 @@ function displayUsers(){
                 [4, 'UserEmail'],
                 [5, 'UserPhoneNo'],
             ]
-        }, 
+        },
         restoreButton: false,
         onSuccess: function (data, textStatus, jqXHR) {
             if (data.action == "delete") {
@@ -21,40 +21,79 @@ function displayUsers(){
     });
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     //Form input dynamic styling
-    $('form input').blur(function(){
-        if(!$(this).val()){
-            $(this).css('border','1px solid red');
-        } else{
-            $(this).css('border','1px solid green');
+    $('form input').blur(function () {
+        if (!$(this).val()) {
+            $(this).css('border', '1px solid red');
+        } else {
+            $(this).css('border', '1px solid green');
         }
     });
 
-    //User Manager
-    $('#searchUID').keyup(function(){
+    /* START OF User Manager */
+    $('#searchUID').keyup(function () {
         var searchUID = $('#searchUID').val();
 
-        $.post('scripts/handlers/searchHandler.php', {searchByUID : searchUID}, function(data){
+        $.post('scripts/handlers/searchHandler.php', { searchByUID: searchUID }, function (data) {
             $('#searchResult').html(data);
             displayUsers();
         });
     });
 
-    $('#searchFName').keyup(function(){
+    $('#searchFName').keyup(function () {
         var searchFName = $('#searchFName').val();
 
-        $.post('./scripts/handlers/searchHandler.php', {searchByFName : searchFName}, function(data){
+        $.post('./scripts/handlers/searchHandler.php', { searchByFName: searchFName }, function (data) {
             $('#searchResult').html(data);
             displayUsers();
         });
     });
+
+    $('#searchLName').keyup(function () {
+        var searchLName = $('#searchLName').val();
+
+        $.post('./scripts/handlers/searchHandler.php', { searchByLName: searchLName }, function (data) {
+            $('#searchResult').html(data);
+            displayUsers();
+        });
+    });
+
+    $('#searchDOB').keyup(function () {
+        var searchDOB = $('#searchDOB').val();
+
+        $.post('./scripts/handlers/searchHandler.php', { searchByDOB: searchDOB }, function (data) {
+            $('#searchResult').html(data);
+            displayUsers();
+        });
+    });
+
+    $('#searchEmail').keyup(function () {
+        var searchEmail = $('#searchEmail').val();
+
+        $.post('./scripts/handlers/searchHandler.php', { searchByEmail: searchEmail }, function (data) {
+            $('#searchResult').html(data);
+            displayUsers();
+        });
+    });
+
+    $('#searchPhoneNo').keyup(function () {
+        var searchPhoneNo = $('#searchPhoneNo').val();
+
+        $.post('./scripts/handlers/searchHandler.php', { searchByPhoneNo: searchPhoneNo }, function (data) {
+            $('#searchResult').html(data);
+            displayUsers();
+        });
+    });
+
+    /* END OF User Manager */
+
 
     displayUsers();
 });
 
 //! Vanila JS (Not in jquery syntax)
-function showToast(){
+function showToast() {
     // Function to be called when login or register
     var login = document.getElementById('login');
     var register = document.getElementById('register');

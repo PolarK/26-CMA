87df4e645d2c9d7f358b77e8c0237314b153c9fe
LenardtFubex <lenardtfu@gmail.com>
2022-09-28 18:50:22 +1000

    Update notification.php
    added notification but not sure if it will sync to the page

--- src/template/notification.php
+++ src/template/notification.php
@@ -34,4 +34,39 @@
             Username and Password do not Match!
         </div>
     </div>
+
+    <!-- Successfull submission -->
+    <div class="toast" aria-live="assertive" aria-atomic="true" id="loginfail">
+        <div class="toast-header">
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="toast-body">
+        <i class="start-icon far fa-check-circle faa-tada animated"></i>WELL DONE! You have submitted the documents.
+        </div>
+    </div>
+
+    <!-- Failed Login Notification -->
+    <div class="toast" aria-live="assertive" aria-atomic="true" id="loginfail">
+        <div class="toast-header">
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="toast-body">
+        <i class="start-icon far fa-times-circle faa-pulse animated"></i>Oh snap! Change a few things up and try submitting again.
+        </div>
+    </div>
+
+    <div class="toast" aria-live="assertive" aria-atomic="true" id="loginfail">
+        <div class="toast-header">
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="toast-body">
+        <i class="start-icon  fa fa-info-circle faa-shake animated"></i>        Heads up! This alert needs your attention
+        </div>
+    </div>
 </div>

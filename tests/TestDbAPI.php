d53451a801679f1c8586a4949f4d0f37be103379
Justin San <santosojustin@gmail.com>
2022-10-18 10:25:23 +1100

    Working on PHPUnit testing on DBAPI class

--- tests/TestDbAPI.php
+++ tests/TestDbAPI.php
@@ -8,12 +8,45 @@ use PHPUnit\Framework\TestCase;
 
 final class TestDbAPI extends TestCase
 {
-    public function __const(){
-        $db = new Database();
+    public function testCreateNewUser() : void
+    {
+        $id = "SRJ-TEST";
+        $firstName = "Josephine";
+        $lastName = "Rodriges";
+        $dob = "01-01-1970";
+        $email = "josephinerodriges@gmail.com";
+        $phone = "0412456789";
+        $role = "SUBMITTER";
+
+        $this->assertTrue(
+            ($this->dbAPI()->createNewUser($id, $firstName, $lastName, $dob, $email, $phone, $role)) ? true : false
+        );
     }
 
-    public function testGetAllUsers() : void
+    public function testFindSpecificUserUsingFirstNameAvailable(): void
     {
+        $this->assertTrue(
+            ($this->dbAPI()->findUserByFirstName("Josephine"))? true : false
+        );
+    }
+
+    public function testFindSpecificUserUsingFirstNameUnavailable(): void
+    {
+        $this->assertFalse(
+            ($this->dbAPI()->findUserByFirstName("Jackovich"))? true : false
+        );
+    }
+
+    public function testRemoveSpecificUser() {
+        $id = "SRJ-TEST";
         
+        $this->assertTrue(
+            ($this->dbAPI()->deleteUser($id)) ? true : false
+        );
+    }
+
+    public function dbAPI(): Database
+    {
+        return new Database();
     }
 }

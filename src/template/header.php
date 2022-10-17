11d9d73fa59b6bb07e3651779d02c3c7cd385a20
JennaCPC <102840208@student.swin.edu.au>
2022-10-18 00:10:30 +1100

    Implemented file size restriction for submissions

--- src/template/header.php
+++ src/template/header.php
@@ -17,6 +17,7 @@ include "./classes/mobile.class.php";
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="scripts\libraries\jquery-toast-plugin\dist\jquery.toast.min.js"></script>
     <script src="scripts\main.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/party-js@latest/bundle/party.min.js"></script>
 </head>
 
 <body>

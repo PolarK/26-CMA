5f1990965f470cec67c63c90b28baf02a366d178
JohnA <90177237+AWildCrow@users.noreply.github.com>
2022-10-20 00:39:01 +1100

    Testing the createNewUser page for errors

--- src/pages/admin/createNewUser.php
+++ src/pages/admin/createNewUser.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once "./classes/dbAPI.class.php";
 require_once "./classes/user.class.php";
 require_once "./classes/validator.class.php";

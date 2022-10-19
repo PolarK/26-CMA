9ab334ca7fd113d407cd8939b18c2c5458cee980
Indra <62127766+PolarK@users.noreply.github.com>
2022-10-19 15:18:02 +1100

    UserClass Unit Test
    a little bit of userclass unit test, far from done more coming

--- tests/TestUserClass.php
+++ tests/TestUserClass.php
@@ -8,7 +8,65 @@ use PHPUnit\Framework\TestCase;
 
 final class TestUserClass extends TestCase
 {
-    public function testUsernameIsValid() : void
+    
+    public function testAllIsValid() : void
+    {
+        User $allvalid = new User("lala12", "lala", "popo", "1974-03-18", "lala@po.com", "0401020202", "ADMIN", 1)
+        $this->assertContains(
+            User::class,
+            User::get_fname("AAJ29b8d")
+        );
+    }
+    
+    public function testFirstnameIsValid() : void
+    {
+        $this->assertContains(
+            User::class,
+            User::get_fname("Joseph")
+        );
+    }
+    
+    public function testLastnameIsValid() : void
+    {
+        $this->assertContains(
+            User::class,
+            User::get_fname("Ayala")
+        );
+    }
+
+    public function testDOBIsValid() : void
+    {
+        $this->assertContains(
+            User::class,
+            User::get_fname("1974-03-18")
+        );
+    }
+
+    public function testEmailIsValid() : void
+    {
+        $this->assertContains(
+            User::class,
+            User::get_fname("1974-03-18")
+        );
+    }
+
+    public function testPhoneIsValid() : void
+    {
+        $this->assertContains(
+            User::class,
+            User::get_fname("1974-03-18")
+        );
+    }
+
+    public function testRoleIsValid() : void
+    {
+        $this->assertContains(
+            User::class,
+            User::get_fname("1974-03-18")
+        );
+    }
+
+    public function TestRegisterIsValid() : void
     {
         $this->assertContains(
             User::class,
